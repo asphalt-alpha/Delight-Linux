a3fdafae21af1e8744cf28fd7a950db702bfb98b
asphalt-alpha <ddolbangpark@gmail.com>
2019-12-14 13:28:27 +0000

    [change] func(on, off) optimization

--- main.c
+++ main.c
@@ -7,14 +7,24 @@
 #include <termio.h>
 #include <assert.h>
 
-#define TEST 1
+#define TEST 0
 #define MAX_LEN 100
+#define DEC 10
+#define HEX 16
+
+typedef struct RGB{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+}RGB;
 
 unsigned long int TimeCompare(int hour, int minuate);
+void SetColor(RGB* rgb, int type, char* r, char* g, char* b);
 void ShowHelp();
 
+
 int main(int argc, char* argv[]){
-    int fd,sfd;
+    int fd, sfd, let;
 
     //data type
     typedef struct ALARM_SCHEDULE{
@@ -25,16 +35,7 @@ int main(int argc, char* argv[]){
     }schedule;
     schedule rec;
 
-    /*
-        fprintf(stderr, "%s Manul : HowToUse\n", argv[0]);
-        fprintf(stderr, "1. set : %s set hour minuate\n", argv[0]);
-        fprintf(stderr, "2. confirm : %s confirm\n", argv[0]);
-        fprintf(stderr, "3. remove : %s remove\n", argv[0]);
-        return 0;
-    */
-
-    //open file
-
+    // Connecting Arduino by UART communication
     if(!(TEST)){
         printf("Initializing...\n");
         sfd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
@@ -52,22 +53,19 @@ int main(int argc, char* argv[]){
         printf("Done.\n");
     }  
 
+    // 2Way-handshaking
     char receive[MAX_LEN];
     int readSize = 0;
-
-    write(sfd,"OK",2);
+    let = write(sfd,"OK",2);
+    assert(let == 2);
     while(1){
         readSize = read(sfd,receive,MAX_LEN);
-        //printf("%d ", readSize);    
         if(readSize == 2){
             printf("%s. Ready.\n",receive);
             break;
         } 
     }
-
-    char *av[10] = {NULL, };
-    int ac = 0, i = 0;
-    char command[MAX_LEN];
+    
 
 
     printf("Enter the Command.\nIf you check the menual, type 'help'.\n");
@@ -78,25 +76,30 @@ int main(int argc, char* argv[]){
             return -1;
         }
 
+        char** av;
+        int ac = 0, i = 0;
+        char command[MAX_LEN];
+
         printf("> ");
         fgets(command,MAX_LEN,stdin);
         command[strlen(command)-1]='\0';
 
         char *ptr;
-        ptr = strtok(command, " ");
-        //ptr = strtok(NULL, " ");
-        ac = 0;
+        ptr = strtok(command, " ");    
         while(ptr != NULL){
             av[i] = ptr;
             i++;    ac++;
             ptr = strtok(NULL, " ");
         }
 
+
+        // Command excute
         if(!(strcmp(av[0], "help"))){
             printf("\n");
             ShowHelp();
             printf("\n");
         }
+
         else if(!(strcmp(av[0], "set"))){
             if(ac!=3){
                 fprintf(stderr, "1. set : %s [hour] [minuate]\n", av[0]);
@@ -108,12 +111,9 @@ int main(int argc, char* argv[]){
                 fprintf(stderr, "err : munuate\n");
                 continue;
             }
-        //set
             rec.startHour=atoi(av[1]);
             rec.startMinate=atoi(av[2]);
-
             //printf("%s %d %d\n",av[1], rec.hour, rec.minuate);
-
             alarm(TimeCompare(rec.startHour, rec.startMinate));
 
             lseek(fd, 0L, SEEK_END);
@@ -124,6 +124,9 @@ int main(int argc, char* argv[]){
                 printf("Setting Failed. Try again.\n");
             }
         }
+
+        
+
         //confirm : exeption
         else if(!(strcmp(av[0], "confirm"))){
             if(ac!=1){
@@ -145,14 +148,45 @@ int main(int argc, char* argv[]){
                 fprintf(stderr, "3. remove : %s", av[0]);
                 return 1;
             }
-        //remove
+            // remove file
             if((remove("./schedule"))==0)printf("remove success\n");
             else printf("remove fail\n");
         }
 
+        //================================================
+
+        else if(!(strcmp(av[0], "on"))){
+            char commandString[MAX_LEN] = {'\0',};
+            RGB color;
+
+            // input anymore
+            if(ac == 1){
+                printf("You are not type Color. Turn on white Color\n");
+                SetColor(&color, HEX, "#C8C8C8", NULL, NULL);
+            }
+            // input dec
+            else if(ac == 4){
+                SetColor(&color, DEC, av[1], av[2], av[3]);
+            }
+            // input hex
+            else if(ac == 2){
+                SetColor(&color, HEX, av[1], NULL, NULL);
+            }
+            sprintf(commandString,"L %d %d %d", color.r, color.g, color.b);
+            let = write(sfd,commandString,strlen(commandString));
+            assert(let == strlen(commandString));
+            printf("Turn on the Light - R:%d G:%d B:%d\n", color.r, color.g, color.b);
+        }
+
+        else if(!(strcmp(av[0], "off"))){
+            printf("Turn off the Light\n");
+            let = write(sfd,"L #000000",9);
+            assert(let == 9);
+        }
     
         else if(!(strcmp(av[0], "exit"))){
             close(fd);
+            printf("Good Bye\n");
             break;
         }
 
@@ -167,6 +201,23 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
+void SetColor(RGB* rgb, int type, char* r, char* g, char* b){
+    if(type == DEC){
+        rgb->r = strtol(r,NULL,10);
+        rgb->g = strtol(g,NULL,10);
+        rgb->b = strtol(b,NULL,10);
+    }
+    else if(type == HEX){
+        char str[10];
+        strcpy(str,r);
+        if(str[0] == '#') str[0] = '0';
+        int data = strtol(str,NULL,16);
+
+        rgb->r = (data&0xFF0000)>>16;
+        rgb->g = (data&0x00FF00)>>8;
+        rgb->b = (data&0x0000FF);
+    }
+}
 
 unsigned long int TimeCompare(int hour, int minuate){
         int chaHour=0, chaMinuate=0;
@@ -186,11 +237,14 @@ unsigned long int TimeCompare(int hour, int minuate){
         return sum;
 }
 
-
 void ShowHelp(){
     printf("This is Automation Control Lamp. You can control the Lamp to the color you wnat.\n");
     fprintf(stderr, "- How to use this program? : help\n");
+    fprintf(stderr, "- Turn on the Light : on [Color(dec or hex)(option)]\n");
+    fprintf(stderr, "- Turn off the Light : off\n");
     fprintf(stderr, "- Setting Lamp Alarm : set [hour] [minuate]\n");
     fprintf(stderr, "- Confirm Lamp Alarm : confirm\n");
     fprintf(stderr, "- Remove Lamp Alarm : remove\n");
+    fprintf(stderr, "- Stop this Program : exit\n");
+
 }
